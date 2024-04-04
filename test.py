2ea7e0c46761a4ecae1dca9d10b8f12cb23ab1e4
ken-miyashita-zeal <162598012+ken-miyashita-zeal@users.noreply.github.com>
2024-04-04 16:35:29 +0900

    Update test.py
    AWS記事が正しく読み込まれるように設定

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # ジールのセットWebページ
-url = 'https://zdh.stagingbridge.net/'
+url = 'https://zdh.stagingbridge.net/topics/'
 
 # ジールのセットWebページにアクセス
 response = requests.get(url)
@@ -15,12 +15,15 @@ soup = BeautifulSoup(response.text, 'html.parser')
 output_path = r'C:\Users\ken.miyashita\Documents\個人データ\Pyhton\test.csv'
 
 # 「AWS」の記載がある記事のタイトルとURLを取得し、test.csvに書き込む
-with open(output_path, 'w', newline='', encoding='utf-8') as csvfile:
+with open(output_path, 'w', newline='', encoding='utf-8-sig') as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(['Title', 'URL'])
     
-    for article in soup.find_all('article'):
-        title = article.find('h2').text
-        url = article.find('a')['href']
-        if 'AWS' in title:
-            csvwriter.writerow([title, url])
+    for article in soup.find_all('a', class_='news-list'):
+        title_elements = article.find_all('p')
+        for title_element in title_elements:
+            if 'AWS' in title_element.text:
+                title = title_element.text
+                url = article['href']
+                csvwriter.writerow([title, url])
+                break  # 記事に関連する属性のループを終了

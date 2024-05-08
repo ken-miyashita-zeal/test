1640a101fd8d3987e8e0bd72bf4ac69c3bcb754e
ken-miyashita-zeal <162598012+ken-miyashita-zeal@users.noreply.github.com>
2024-05-08 16:20:15 +0900

    不要なコードの削除

--- test.py
+++ test.py
@@ -2,28 +2,25 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-# ジールのセットWebページ
-url = 'https://zdh.stagingbridge.net/topics/'
+# ジールのセットWebページからAWSを検索したWebページ
+search_url = 'https://zdh.stagingbridge.net/?s=aws'
 
 # ジールのセットWebページにアクセス
-response = requests.get(url)
+search_response = requests.get(search_url)
 
 # ページの内容を解析
-soup = BeautifulSoup(response.text, 'html.parser')
+search_soup = BeautifulSoup(search_response.text, 'html.parser')
 
 # CSVファイルの出力先パス
 output_path = r'C:\Users\ken.miyashita\Documents\個人データ\Pyhton\TEST\test.csv'
 
-# 「AWS」の記載がある記事のタイトルとURLを取得し、test.csvに書き込む
+# CSVファイルを読み込み、ヘッダーの書き込み
 with open(output_path, 'w', newline='', encoding='utf-8-sig') as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(['Title', 'URL'])
-    
-    for article in soup.find_all('a', class_='news-list'):
-        title_elements = article.find_all('p')
-        for title_element in title_elements:
-            if 'AWS' in title_element.text:
-                title = title_element.text
-                url = article['href']
-                csvwriter.writerow([title, url])
-                break  # 記事に関連する属性のループを終了
+
+    # 検索結果のページから、タイトルとURLを取得してCSVファイルに書き込む
+    for item in search_soup.find_all('div', class_='result-list'):
+        title = item.find('span', class_='ttl').text.strip()
+        url = item.find('a')['href']
+        csvwriter.writerow([title, url])
